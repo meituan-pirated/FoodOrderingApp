26e6eac8efbaec099c2d30cfe2b288526489c1f0
jasminezhao324 <52599324+jasminezhao324@users.noreply.github.com>
2021-05-08 21:09:04 +0800

    main主页

--- app/src/main/java/com/example/foododeringapp/merchant/Activity_Merchant_Main.java
+++ app/src/main/java/com/example/foododeringapp/merchant/Activity_Merchant_Main.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.foododeringapp.R;
+import com.example.foododeringapp.control.BaseActivity;
 
-public class Activity_Merchant_Main extends AppCompatActivity {
+public class Activity_Merchant_Main extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__merchant__main);
+
     }
 }
